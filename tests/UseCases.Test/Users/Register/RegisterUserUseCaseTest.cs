2202fe9c1b97e7b804c5d26682b68438c852cb32
Leonardo <leonardohmgussi@gmail.com>
2026-08-08 11:00:09 -0300

    tests: finishing the test for success for register user use case

--- tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -9,6 +9,7 @@ using Shouldly;
 namespace UseCases.Test.Users.Register;
 public class RegisterUserUseCaseTest
 {
+    [Fact]
     public async Task Success()
     {
         var request = RequestRegisterUserJsonBuilder.Build();
@@ -28,7 +29,8 @@ public class RegisterUserUseCaseTest
         var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
         var passwordEncripter = PasswordEncripterBuilder.Build();
         var tokenGenerator = JwtTokenGeneratorBuilder.Build();
+        var readRepository = new UserReadOnlyRepositoryBuilder().Build();
 
-        return new RegisterUserUseCase(mapper, passwordEncripter, null, writeRepository, tokenGenerator, unitOfWork);
+        return new RegisterUserUseCase(mapper, passwordEncripter, readRepository, writeRepository, tokenGenerator, unitOfWork);
     }
 }
