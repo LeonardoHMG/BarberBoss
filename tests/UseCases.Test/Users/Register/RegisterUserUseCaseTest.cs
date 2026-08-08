58c3670cc59b597048aa26080e4633875fa4e653
Leonardo <leonardohmgussi@gmail.com>
2026-08-08 10:04:13 -0300

    tests: starting usecases test

---
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,24 @@
+﻿using BarberBoss.Application.UseCases.Users.Register;
+using CommonTestUtilities.Requests;
+using Shouldly;
+
+namespace UseCases.Test.Users.Register;
+public class RegisterUserUseCaseTest
+{
+    public async Task Success()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var useCase = CreateUseCase();
+
+        var result = await useCase.Execute(request);
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe(request.Name);
+        result.Token.ShouldNotBeNullOrWhiteSpace();
+    }
+
+    private RegisterUserUseCase CreateUseCase()
+    {
+        return new RegisterUserUseCase(null, null, null, null, null, null);
+    }
+}
