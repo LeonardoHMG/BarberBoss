da1015ed14821d6ae5cef66e56b8e74b712816a7
Leonardo <leonardohmgussi@gmail.com>
2026-08-08 13:49:12 -0300

    tests: starting unit test for login usecase

---
+++ tests/CommonTestUtilities/Entities/UserBuilder.cs
@@ -0,0 +1,24 @@
+﻿using BarberBoss.Domain.Entities;
+using BarberBoss.Domain.Enums;
+using Bogus;
+using CommonTestUtilities.Cryptography;
+
+namespace CommonTestUtilities.Entities;
+public class UserBuilder
+{
+    public static User Build(string role = null)
+    {
+        var passwordEncripter = PasswordEncripterBuilder.Build();
+
+        var user = new Faker<User>()
+            .RuleFor(u => u.Id, _ => Guid.NewGuid())
+            .RuleFor(u => u.Name, faker => faker.Person.FirstName)
+            .RuleFor(u => u.Email, (faker, user) => faker.Internet.Email(user.Name))
+            .RuleFor(u => u.PasswordHash, (_, user) => passwordEncripter.Encrypt(user.Name))
+            .RuleFor(u => u.Role, _ => role ?? Roles.BARBER)
+            .RuleFor(u => u.CreatedAt, faker => faker.Date.PastOffset().UtcDateTime)
+            .RuleFor(u => u.UpdatedAt, (_, user) => user.CreatedAt);
+        
+        return user;
+    }
+}
---
+++ tests/UseCases.Test/Login/DoLogin/DoLoginUseCaseTest.cs
@@ -0,0 +1,71 @@
+﻿using BarberBoss.Application.UseCases.Login.DoLogin;
+using BarberBoss.Domain.Entities;
+using BarberBoss.Exception;
+using BarberBoss.Exception.ExceptionsBase;
+using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Token;
+using Shouldly;
+
+namespace UseCases.Test.Login.DoLogin;
+public class DoLoginUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+
+        var request = RequestLoginJsonBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        var result = await useCase.Execute(request);
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe(user.Name);
+        result.Token.ShouldNotBeNullOrWhiteSpace();
+    }
+
+    [Fact]
+    public async Task Error_User_Not_Found()
+    {
+        var user = UserBuilder.Build();
+
+        var request = RequestLoginJsonBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        var act = async () => await useCase.Execute(request);
+
+        var exception = await Should.ThrowAsync<InvalidLoginException>(act);
+        exception.GetErrors().Count.ShouldBe(1);
+        exception.GetErrors().ShouldContain(ResourceErrorMessages.EMAIL_OR_PASSWORD_INVALID);
+    }
+
+    [Fact]
+    public async Task Error_Password_Not_Match()
+    {
+        var user = UserBuilder.Build();
+
+        var request = RequestLoginJsonBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        var act = async() => await useCase.Execute(request);
+
+        var exception = await Should.ThrowAsync<ErrorOnValidationException>(act);
+        exception.GetErrors().Count.ShouldBe(1);
+        exception.GetErrors().ShouldContain(ResourceErrorMessages.EMAIL_OR_PASSWORD_INVALID);
+    }
+
+    private DoLoginUseCase CreateUseCase(User user)
+    {
+        var passwordEncripter = PasswordEncripterBuilder.Build();
+        var tokenGenerator = JwtTokenGeneratorBuilder.Build();
+        var readRepository = new UserReadOnlyRepositoryBuilder().GetUserByEmail(user).Build();
+
+        return new DoLoginUseCase(readRepository, passwordEncripter, tokenGenerator);
+    }
+}
