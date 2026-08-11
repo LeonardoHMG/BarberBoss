4d35b817a5435e5324cbbcafbcdc64d00f5a06fd
Leonardo <leonardohmgussi@gmail.com>
2026-08-11 18:21:55 -0300

    test: adding integration test for login

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,7 @@
-﻿using BarberBoss.Infrastructure.DataAccess;
+﻿using BarberBoss.Domain.Entities;
+using BarberBoss.Domain.Security.Cryptography;
+using BarberBoss.Infrastructure.DataAccess;
+using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +10,9 @@ using Microsoft.Extensions.DependencyInjection;
 namespace WebApi.Test;
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private User _user;
+    private string _password;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
@@ -19,6 +25,28 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                     config.UseInMemoryDatabase("InMemoryDbForTesting");
                     config.UseInternalServiceProvider(provider);
                 });
+
+                var scope = services.BuildServiceProvider().CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<BarberBossDbContext>();
+                var passwordEncripter = scope.ServiceProvider.GetRequiredService<IPasswordEncripter>();
+
+                StartDatabase(dbContext, passwordEncripter);
             });
+    }
+
+    public string GetName() => _user.Name;
+    public string GetEmail() => _user.Email;
+    public string GetPassword() => _password;
+
+    private void StartDatabase(BarberBossDbContext dbContext, IPasswordEncripter passwordEncripter)
+    {
+        _user = UserBuilder.Build();
+        _password = _user.PasswordHash;
+
+        _user.PasswordHash = passwordEncripter.Encrypt(_user.PasswordHash);
+
+        dbContext.Users.Add(_user);
+
+        dbContext.SaveChanges();
     }
 }
---
+++ tests/WebApi.Test/Login/DoLogin/DoLoginTest.cs
@@ -0,0 +1,66 @@
+﻿using BarberBoss.Communication.Requests;
+using BarberBoss.Exception;
+using CommonTestUtilities.Requests;
+using Shouldly;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace WebApi.Test.Login.DoLogin;
+public class DoLoginTest: IClassFixture<CustomWebApplicationFactory>
+{
+    private const string METHOD = "api/Login";
+
+    private readonly HttpClient _httpClient;
+    private readonly string _email;
+    private readonly string _name;
+    private readonly string _password;
+
+    public DoLoginTest(CustomWebApplicationFactory webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();
+        _email = webApplicationFactory.GetEmail();
+        _name = webApplicationFactory.GetName();
+        _password = webApplicationFactory.GetPassword();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = new RequestLoginJson
+        {
+            Email = _email,
+            Password = _password
+        };
+
+        var response = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.GetProperty("name").GetString().ShouldBe(_name);
+
+        responseData.RootElement.GetProperty("token").GetString().ShouldNotBeNullOrWhiteSpace();
+    }
+
+    [Fact]
+    public async Task Error_Login_Invalid()
+    {
+        var request = RequestLoginJsonBuilder.Build();
+
+        var response = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+
+        var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = responseData.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        errors.ShouldHaveSingleItem().GetString()!.ShouldBe(ResourceErrorMessages.EMAIL_OR_PASSWORD_INVALID);
+    }
+}
